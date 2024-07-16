cb68a1e5e69080565b3a5ff9bfc16694de1eed54
Rapelli Naga Sathvik <111189211+Nagasathvik@users.noreply.github.com>
2024-07-16 15:15:19 +0530

    Update svm.py

--- 2. SVM/svm.py
+++ 2. SVM/svm.py
@@ -3,7 +3,8 @@ import numpy as np
 import pickle
 from sklearn import model_selection, svm, preprocessing
 from sklearn.metrics import accuracy_score,confusion_matrix
-from MNIST_Dataset_Loader.mnist_loader import MNIST
+# from MNIST_Dataset_Loader.mnist_loader import MNIST
+from mnist_loader import MNIST
 import matplotlib.pyplot as plt
 from matplotlib import style
 style.use('ggplot')
